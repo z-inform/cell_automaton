3f712f8fbc4b3e0c6fd85066c5d7edb9e4939c13
Zakhar <z.inform@mail.ru>
2021-12-30 17:45:34 +0300

    rule structs added

---
+++ rule.h
@@ -0,0 +1,21 @@
+struct CoordPair {
+    char x;
+    char y;
+};
+
+struct CoordArr {
+    unsigned char cell_type;
+    struct CoordPair* cell_coords;
+};
+
+struct CellNumber {
+    unsigned char cell_type;
+    unsigned char cell_number;
+}
+
+struct RuleStruct {
+    unsigned char target_type;
+    struct CoordArr* coord_rules;
+    struct CellNumber* number_rules;
+};
+
